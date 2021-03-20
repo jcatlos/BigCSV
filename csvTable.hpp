e06fe4dee9f1f54d18ecf1bc78116efc64884cf6
Jakub Čatloš <jakub.catlos@gmail.com>
2021-03-20 19:20:25 +0100

    Endline is now char, updated constructor

--- csvTable.hpp
+++ csvTable.hpp
@@ -17,13 +17,14 @@ namespace bigCSV{
 
         char delimiter;
         char quotechar;
-        std::string endline;
+        char endline;
 
         std::vector<std::string> columns;
         std::vector<csvFile> input_files;
         std::vector<bool> extends_schema;           // extends_schema[i] is true when
 
-        csvTable() : delimiter(','), quotechar('"'), endline("\r\n"), columns(), input_files(){};
+        csvTable(char delim, char qchar, char endl) : delimiter(delim), quotechar(qchar), endline(endl), columns(), input_files(){};
+
 
         void addStream(std::string path, char delimiter, char endline, char quotechar, bool extends);
 
